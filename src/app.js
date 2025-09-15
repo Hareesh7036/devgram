5020e00128a3d7ff98f1f5ccacb77e768e8ea354
Hareesh7036 <madahareesh12@gmail.com>
2025-09-15 22:52:26 +0530

    create user model and create a route for signup with dummy data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const app = express();
+const User = require("./models/user");
 const connectDB = require("./config/database");
 
 app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Hari",
+    lastName: "Avvari",
+    emailId: "hari@gmail.com",
+    password: "Hari@123",
+    age: 27,
+  });
+  try {
+    await user.save();
+    res.send("User signed up successfully");
+  } catch (err) {
+    console.error("Error saving user:", err);
+    res.status(500).send("Error saving user");
+  }
+});
 
 connectDB()
   .then(() => {
