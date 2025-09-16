5144a0dbf93dfca9f25615e523d4b29898ea737c
Hareesh7036 <madahareesh12@gmail.com>
2025-09-16 22:34:22 +0530

    write few api's to get, update, delete etc..

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   const { firstName, lastName, emailId, password, age } = req.body;
+
+  // instance of the user model
   const user = new User({
     firstName,
     lastName,
@@ -20,6 +22,75 @@ app.post("/signup", async (req, res) => {
   } catch (err) {
     console.error("Error saving user:", err);
     res.status(500).send("Error saving user");
+  }
+});
+// get user by email id..
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const feed = await User.find({});
+    res.send(feed);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.id;
+  const newAge = req.body.age;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      { _id: userId },
+      { age: newAge }
+    );
+    if (!updatedUser) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+// update user by email id
+app.patch("/userbyemail", async (req, res) => {
+  const email = req.body.emailId;
+  const newAge = req.body.age;
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { emailId: email },
+      { age: newAge }
+    );
+    if (!updatedUser) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
   }
 });
 
