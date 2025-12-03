de89c29b2541193136de000d586ff76b398e945b
Hareesh7036 <madahareesh12@gmail.com>
2025-12-03 22:34:29 +0530

    add allowed origin

--- src/app.js
+++ src/app.js
@@ -16,7 +16,21 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://devgram-frontend.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+      else callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/", authRouter);
 
